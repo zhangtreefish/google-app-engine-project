de468047010fb6089ebaa707fc3662bcf630f40c
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-01-17 22:04:53 -0600

    feat:add front-end elements and associated js button control  to showcase the new endpoint sayHelloByPeriod

--- Lesson_2/000_Hello_Endpoints/static/hello.js
+++ Lesson_2/000_Hello_Endpoints/static/hello.js
@@ -11,16 +11,16 @@
  */
 
 function init() {
-	
+
 	// You need to pass the root path when you load your API
 	// otherwise calls to execute the API run into a problem
-	
+
 	// rootpath will evaulate to either of these, depending on where the app is running:
 	// //localhost:8080/_ah/api
 	// //your-app-id/_ah/api
 
 	var rootpath = "//" + window.location.host + "/_ah/api";
-	
+
 	// Load the helloworldendpoints API
 	// If loading completes successfully, call loadCallback function
 	gapi.client.load('helloworldendpoints', 'v1', loadCallback, rootpath);
@@ -28,12 +28,12 @@ function init() {
 
 /*
  * When helloworldendpoints API has loaded, this callback is called.
- * 
+ *
  * We need to wait until the helloworldendpoints API has loaded to
  * enable the actions for the buttons in index.html,
  * because the buttons call functions in the helloworldendpoints API
  */
-function loadCallback () {	
+function loadCallback () {
 	// Enable the button actions
 	enableButtons ();
 }
@@ -42,16 +42,19 @@ function enableButtons () {
 	// Set the onclick action for the first button
 	btn = document.getElementById("input_greet_generically");
 	btn.onclick= function(){greetGenerically();};
-	
 	// Update the button label now that the button is active
 	btn.value="Click me for a generic greeting";
-	
+
 	// Set the onclick action for the second button
 	btn = document.getElementById("input_greet_by_name");
 	btn.onclick=function(){greetByName();};
-	
 	// Update the button label now that the button is active
 	btn.value="Click me for a personal greeting";
+
+	//I added here: the onclick action for the second button
+	btn = document.getElementById("input_greet_by_period");
+	btn.onclick=function(){greetByPeriod();};
+	btn.value="Click me for a personal period greeting";
 }
 
 /*
@@ -60,7 +63,7 @@ function enableButtons () {
 function greetGenerically () {
 	// Construct the request for the sayHello() function
 	var request = gapi.client.helloworldendpoints.sayHello();
-	
+
 	// Execute the request.
 	// On success, pass the response to sayHelloCallback()
 	request.execute(sayHelloCallback);
@@ -73,7 +76,7 @@ function greetGenerically () {
 function greetByName () {
 	// Get the name from the name_field element
 	var name = document.getElementById("name_field").value;
-	
+
 	// Call the sayHelloByName() function.
 	// It takes one argument "name"
 	// On success, pass the response to sayHelloCallback()
@@ -81,11 +84,23 @@ function greetByName () {
 	request.execute(sayHelloCallback);
 }
 
+function greetByPeriod () {
+	// Get the name from the name_field element
+	var name = document.getElementById("name_field").value;
+
+	// Call the sayHelloByName() function.
+	// It takes one argument "name"
+	// On success, pass the response to sayHelloCallback()
+	var period = document.getElementById("period_field").value;
+	var request = gapi.client.helloworldendpoints.sayHelloByPeriod({'period':period,'name': name});
+	request.execute(sayHelloCallback);
+}
+
 // Process the JSON response
 // In this case, just show an alert dialog box
 // displaying the value of the message field in the response
 function sayHelloCallback (response) {
-	alert(response.greeting);	
+	alert(response.greeting);
 }
 
 
