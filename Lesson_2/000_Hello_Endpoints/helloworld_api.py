0f334d9f23342a20100b775bcf5422ff21cab46a
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-01-17 22:02:31 -0600

    feat:add a new endpoint sayHelloByPeriod

--- Lesson_2/000_Hello_Endpoints/helloworld_api.py
+++ Lesson_2/000_Hello_Endpoints/helloworld_api.py
@@ -17,30 +17,39 @@ REQUEST_CONTAINER = endpoints.ResourceContainer(
     message_types.VoidMessage,
     name=messages.StringField(1),
 )
-
+REQUEST_GREET_CONTAINER = endpoints.ResourceContainer(
+    name=messages.StringField(1),
+    period=messages.StringField(2),
+)
 
 package = 'Hello'
 
 
-class Hello(messages.Message):
+class HelloResponse(messages.Message):
     """String that stores a message."""
-    greeting = messages.StringField(1)
+    greeting = messages.StringField(1,required=True)
 
-
-@endpoints.api(name='helloworldendpoints', version='v1')
+# Define a subclass of remote.Service and decorate it with @endpoints.api
+@endpoints.api(name='helloworldendpoints', version='v1',description='tf api')
 class HelloWorldApi(remote.Service):
     """Helloworld API v1."""
 
-    @endpoints.method(message_types.VoidMessage, Hello,
-      path = "sayHello", http_method='GET', name = "sayHello")
+    @endpoints.method(message_types.VoidMessage, HelloResponse,
+        path = "sayHello", http_method='GET', name = "sayHello")
     def say_hello(self, request):
-      return Hello(greeting="Hello World")
+        return HelloResponse(greeting="Hello World this is 9pm")
 
-    @endpoints.method(REQUEST_CONTAINER, Hello,
-      path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
+    @endpoints.method(REQUEST_CONTAINER, HelloResponse,
+        path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
     def say_hello_by_name(self, request):
-      greet = "Hello {}".format(request.name)
-      return Hello(greeting=greet)
+        greet = "Guten {}".format(request.name)
+        return HelloResponse(greeting=greet)
+
+    @endpoints.method(REQUEST_GREET_CONTAINER, HelloResponse,
+      path = "sayHelloByPeriod", http_method='GET', name = "sayHelloByPeriod")
+    def say_hello_by_period(self, request):
+        greet = "Guten {} {}".format(request.period,request.name)
+        return HelloResponse(greeting=greet)
 
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
