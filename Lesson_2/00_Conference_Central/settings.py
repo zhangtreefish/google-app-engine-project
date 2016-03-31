ee4f92f5762efd9b84ddf4607dbaba34b69e2309
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-03-31 10:43:12 -0500

    fix: initialize client ids for other devices

--- Lesson_2/00_Conference_Central/settings.py
+++ Lesson_2/00_Conference_Central/settings.py
@@ -6,11 +6,15 @@ Udacity conference server-side Python App Engine app user settings
 
 $Id$
 
-created/forked from conference.py by wesc on 2014 may 24
+created/forked from conference.py by wesc on 2014 may 24, forked/modified by zhangs in
+2016
 
 """
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.For each new application launched from Google App Engine Launcher,
 # get a new one from API manager/credentials/oauthconsent screen at the google console
-WEB_CLIENT_ID = '47174006042-hkms0q2ud7c5tc2t4n5euhhprc5qp94k.apps.googleusercontent.com'
+WEB_CLIENT_ID = '174131492121-1hplaud8jalbv9edkkl2umg1of48dtd7.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'replace with Android client ID'
+IOS_CLIENT_ID = 'replace with iOS client ID'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
